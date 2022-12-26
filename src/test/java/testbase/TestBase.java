545789e0524d9cb4fa4cc3cde22dd6738dacd4d5
Spitsyn Kirill <kaspicin2602@gmail.com>
2022-12-26 13:11:02 +0300

    add baseUrl

--- src/test/java/testbase/TestBase.java
+++ src/test/java/testbase/TestBase.java
@@ -17,9 +17,10 @@ public class TestBase extends TestData {
     @BeforeAll
     static void setup() {
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.baseUrl = System.getProperty("baseUrl","https://demoqa.com");
         Configuration.browserVersion = System.getProperty("browserVersion");
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/") + "wd/hub";
+        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud") + "/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
