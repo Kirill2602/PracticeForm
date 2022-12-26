ec40892d6f3fe5e1be64fd1e927571f399b05654
Spitsyn Kirill <kaspicin2602@gmail.com>
2022-12-26 15:08:52 +0300

    commit

--- src/test/java/testbase/TestBase.java
+++ src/test/java/testbase/TestBase.java
@@ -8,7 +8,6 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import testdata.TestData;
 
@@ -16,22 +15,10 @@ public class TestBase extends TestData {
     @Step("Подготовка")
     @BeforeAll
     static void setup() {
-        String browserSize = System.getProperty("browserSize");
-        String baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
-        String browserVersion = System.getProperty("browserVersion");
-        String browser = System.getProperty("browser");
-        String remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud") + "/wd/hub";
-
         Configuration.browserSize = System.getProperty("browserSize");
-        System.out.println(browserSize);
         Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
-        System.out.println(baseUrl);
-        Configuration.browserVersion = System.getProperty("browserVersion");
-        System.out.println(browserVersion);
         Configuration.browser = System.getProperty("browser");
-        System.out.println(browser);
         Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud") + "/wd/hub";
-        System.out.println(remote);
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
